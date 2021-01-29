ca764f65549c5bf7d12c0faf9fb0d1396d68b8eb
mark1879 <mark1879@163.com>
2021-01-29 09:48:02 +0800

    查找回文子串，增加中心扩展法

--- CPP/LeetCode/LeetCode/longest_palindrome.hpp
+++ CPP/LeetCode/LeetCode/longest_palindrome.hpp
@@ -24,6 +24,7 @@ public:
         return true;
     }
     
+    // 暴力搜索
     static string Solution1(string s) {
         int size = (int)s.size();
         
@@ -31,7 +32,6 @@ public:
             return "";
         
         string longest_palindrome;
-        
         int start = 0, end = 0;
         
         for(unsigned i = (size -1); i >= 0; i--){
@@ -42,7 +42,6 @@ public:
             
             while (i - ret > end - start){
                 if (IsPalindrome(s, (int)ret, i)){
-//                    longest_palindrome = s.substr(ret, (i - ret + 1));
                     start = (int)ret;
                     end = i;
                     break;
@@ -57,8 +56,86 @@ public:
     
     static void TestSolution1(){
         cout << Solution1("eabcb") << endl;
+        cout << Solution1("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgx") << endl;
+    }
+    
+    
+    // 动态规划
+    static string Solution2(string s){
+        int size = (int)s.size();
+        vector<vector<int>> dp(size, vector<int>(size));
         
-//        cout << Solution1("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgx") << endl;
+        int start = 0, end = 0;
+        
+        for (int l = 0; l < size; ++l) {
+            for (int i = 0; i + l < size; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = 1;
+                } else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                
+                if (dp[i][j] && (l > end - start)) {
+                    start = i;
+                    end = l;
+                }
+            }
+        }
+        return s.substr(start, end + 1);
+    }
+    
+    static void TestSolution2(){
+        cout << Solution2("eabcb") << endl;
+        cout << Solution2("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgx") << endl;
+
+    }
+    
+    static pair<int, int> expandAroundCenter(const string& s, int left, int right) {
+        int size = (int)s.size();
+        
+        while (left >= 0 && right < size && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        
+        return {++left, --right};
+    }
+    
+    // 中心扩散法
+    static string Solution3(string s){
+        if (s.size() == 0){
+            return "";
+        }
+        
+        int start = 0, end = 0;
+        
+        for (int i = 0, size = (int)s.size(); i < size; i++){
+            pair<int, int> ret1 = expandAroundCenter(s, i, i);
+            pair<int, int> ret2 = expandAroundCenter(s, i, i+1);
+            
+            if (ret1.second - ret1.first > end - start){
+                start = ret1.first;
+                end = ret1.second;
+            }
+            
+            if (ret2.second - ret2.first > end - start){
+                start = ret2.first;
+                end = ret2.second;
+            }
+        }
+        
+        return s.substr(start, end - start + 1);
+    }
+    
+    static void TestSolution3(){
+        cout << Solution3("") << endl;
+        cout << Solution3("a") << endl;
+        cout << Solution3("ab") << endl;
+        cout << Solution3("aa") << endl;
+        cout << Solution3("anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgx") << endl;
     }
 };
 
